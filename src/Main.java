a577a081d3fba53a2583b79dca0bc220da54387f
Mujib517 <mujib517@gmail.com>
2018-02-10 21:36:04 +0530

    Stock span with stack

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
+        Stacks.Problems.stockSpan2(new int[]{100, 80, 60, 70, 60, 75, 85}, 7);
+        Stacks.Problems.stockSpan2(new int[]{10, 4, 5, 90, 120, 80}, 6);
+        Stacks.Problems.stockSpan2(new int[]{0, 7, 3, 6, 6, 9, 18, 0, 16, 0}, 10);
+        System.out.println();
+        Stacks.Problems.stockSpan(new int[]{10, 4, 5, 90, 120, 80});
+        System.out.println();
+        Stacks.Problems.stockSpan(new int[]{0, 7, 3, 6, 6, 9, 18, 0, 16, 0});
+
     }
 }
 
--- src/Stacks/Problems.java
+++ src/Stacks/Problems.java
@@ -60,12 +60,43 @@ public class Problems {
         for (int i = 0; i < arr.length; i++) {
             int j = i - 1;
             int span = 1;
-            while (j >= 0) {
-                if (arr[i] >= arr[j]) span++;
+            while (j >= 0 && arr[i] >= arr[j]) {
+                span++;
                 j--;
             }
 
             System.out.print(span + " ");
         }
+    }
+
+    public static void stockSpan2(int[] arr, int n) {
+        if (n == 0) return;
+
+        Stack<Item> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (s.isEmpty()) {
+                s.push(new Item(arr[i], 1));
+                System.out.print("1 ");
+                continue;
+            }
+            Item top = s.peek();
+            if (top.data > arr[i]) {
+                s.push(new Item(arr[i], 1));
+                System.out.print("1 ");
+            } else {
+                int span = 1;
+
+                while (!s.isEmpty() && s.peek().data <= arr[i]) {
+                    Item it = s.pop();
+                    int ct = it.min;
+                    span += ct;
+                }
+                s.push(new Item(arr[i], span));
+                System.out.print(span + " ");
+            }
+        }
+
+        System.out.println();
     }
 }
