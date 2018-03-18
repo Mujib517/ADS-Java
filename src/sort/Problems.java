74c7cc9ffc35d7cf699c5f9b10d704cb566dea5a
Mujib517 <mujib517@gmail.com>
2018-03-18 12:33:00 +0530

    Merge sort

--- src/sort/Problems.java
+++ src/sort/Problems.java
@@ -76,6 +76,39 @@ public class Problems {
     }
 
 
+    public static void mergeSort(int[] arr, int n) {
+        int[] temp = new int[n];
+        mergeSort(arr, temp, 0, n - 1);
+    }
+
+    private static void mergeSort(int[] arr, int[] temp, int low, int high) {
+
+        if (low == high) return;
+
+        int mid = (low + high) / 2;
+        mergeSort(arr, temp, low, mid);
+        mergeSort(arr, temp, mid + 1, high);
+        merge(arr, temp, low, high);
+
+    }
+
+
+    private static void merge(int[] arr, int[] c, int low, int high) {
+
+        int mid = (low + high) / 2;
+
+        int i = low, j = mid + 1, k = low;
+
+        while (i <= mid && j <= high) c[k++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+
+        while (i <= mid) c[k++] = arr[i++];
+
+        while (j <= high) c[k++] = arr[j++];
+
+        System.arraycopy(c, low, arr, low, high - low + 1);
+    }
+
+
     private static void printArray(int[] arr, int n) {
         for (int el : arr) {
             System.out.print(el + " ");
