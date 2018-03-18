d4100c62348bcb5eb87ee1e81281a3180c464c3c
Mujib517 <mujib517@gmail.com>
2018-03-18 15:56:57 +0530

    Sqrt

--- src/searching/Problems.java
+++ src/searching/Problems.java
@@ -19,13 +19,38 @@ public class Problems {
         int low = 0;
         int high = n - 1;
 
-        while (low <=high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] > k) high = mid - 1;
             else if (arr[mid] < k) low = mid + 1;
             else return true;
         }
         return false;
+    }
+
+    //O(N)
+    public static int sqrt(int n) {
+        int max = n / 2;
+        for (int i = 0; i < max; i++) {
+            if (i * i == n) return i;
+        }
+        return -1;
+    }
+
+    public static int sqrt2(int n) {
+        int lo = 0;
+        int hi = n / 2;
+
+        while (lo <= hi) {
+
+            int mid = lo + (hi - lo) / 2;
+            int val = mid * mid;
+
+            if (val < n) lo = mid + 1;
+            else if (val > n) hi = mid - 1;
+            else return mid;
+        }
+        return -1;
     }
 
     private static boolean binSearch(int[] arr, int k, int start, int end) {
