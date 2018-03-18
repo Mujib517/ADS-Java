61abae91feac22ce0f36a92b3cb24ee00c8f28a5
Mujib517 <mujib517@gmail.com>
2018-03-18 16:05:39 +0530

    First missing number

--- src/searching/Problems.java
+++ src/searching/Problems.java
@@ -53,6 +53,22 @@ public class Problems {
         return -1;
     }
 
+    // arr: 1,2,3,4,7,10,12
+    // arr[i]=i+1
+    public static int findFirstMissingNumber(int[] arr, int n) {
+        int lo = 0, hi = n - 1;
+        int mid = 0;
+
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] == mid + 1) lo = mid + 1;
+            else if (arr[mid] > mid + 1) hi = mid - 1;
+        }
+
+        return mid + 1;
+    }
+
     private static boolean binSearch(int[] arr, int k, int start, int end) {
 
         if (start > end) return false;
