21780e125a42f3ceb4ae955b68e97e34d747fff1
Mujib517 <Mujib517@gmail.com>
2018-09-18 16:42:39 +0530

    Matrix problems

--- src/arrays/Problems.java
+++ src/arrays/Problems.java
@@ -12,4 +12,43 @@ public class Problems {
         }
         return result;
     }
+
+    public static void printMatrixSpiralOrder(int[][] matrix) {
+        boolean ltr = true;
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            if (ltr) {
+                int j = 0;
+
+                while (j < matrix[0].length) {
+                    System.out.print(matrix[i][j] + " ");
+                    j++;
+                }
+            } else {
+                int j = matrix[0].length - 1;
+                while (j >= 0) {
+                    System.out.print(matrix[i][j] + " ");
+                    j--;
+                }
+            }
+            System.out.println();
+            ltr = !ltr;
+        }
+    }
+
+    public static int[][] matrixMultiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+
+        return c;
+    }
 }
