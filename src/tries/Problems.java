c4873205af75217813f8f1ef92b10a3fd03cfd3a
Mujib517 <Mujib517@gmail.com>
2018-09-18 14:54:17 +0530

    TRIE Problem

---
+++ src/tries/Problems.java
@@ -0,0 +1,22 @@
+package tries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problems {
+
+    //given a string find whether a string can be broken into words
+    public static boolean canSeparateWords(TRIE t, String str) {
+        boolean wordFound = false;
+        String strToFind = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            strToFind += str.charAt(i);
+            wordFound = t.find(strToFind);
+            if(wordFound) strToFind="";
+            if (i == str.length() - 1) return wordFound;
+        }
+
+        return wordFound;
+    }
+}
